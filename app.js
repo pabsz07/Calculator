cf7b5b1bc8081edaed4d096b1bd2a75f41997a53
Michael Pabs <pabsz07mike@gmail.com>
2022-04-10 19:36:31 +0800

    Added equal function statements and delete function statements

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ let isFirstOperand = false;
 let isOperator = false;
 let isSecondOperand = false;
 let length;
-let isAppend = false;
+let isConcat = false;
+let isClicked = false;
+let isEquated = false;
 
 function getLength(arr) {
     return arr.length - 1;
@@ -15,12 +17,17 @@ const calculator = {
 
     subtract(x, y) { return x - y; },
 
-    multiply(x, y) { return x * y; },
+    multiply(x, y) {
+
+        if (x * y === NaN || x * y === undefined)
+            return "ERROR"
+        else
+            return x * y;
+    },
 
     divide(x, y) {
-        if (y === 0) {
+        if (x / y === Infinity || x / y === NaN)
             return "ERROR"
-        }
         else return x / y;
     },
 }
@@ -42,24 +49,52 @@ nums.forEach(num => num.addEventListener('click', addtoScreen));
 
 function addtoScreen(e) {
 
-    if (isAppend) { screen.textContent += e.target.textContent; }
+    isClicked = true;
+
+    if (isConcat) { screen.textContent += e.target.textContent; }
     else {
-        screen.textContent='';
+        screen.textContent = '';
         screen.textContent += e.target.textContent;
-        isAppend=true;
+        isConcat = true;
     }
 }
 
 const clear = document.getElementById("clear");
-clear.addEventListener('click', () => screen.textContent = '');
+clear.addEventListener('click', () => {
+    screen.textContent = '';
+    equation.length = 0;
+    isConcat = false;
+    isClicked = false;
+    isFirstOperand = false;
+    isOperator = false;
+    isSecondOperand = false;
+}
+);
 
 const del = document.getElementById("delete");
-del.addEventListener('click', () => screen.textContent = screen.textContent.slice(0, -1));
+del.addEventListener('click', () => {
+
+    screen.textContent = screen.textContent.slice(0, -1);
+    if (isEquated) {
+        isFirstOperand = false;
+        isOperator = false;
+        isSecondOperand = false;
+        isEquated = false;
+        equation.length = 0;
+    }
+});
 
 const operators = document.querySelectorAll('button.operator');
 operators.forEach(operator => operator.addEventListener('click', calculate));
 
 function calculate(e) {
+
+    if (!isClicked) {
+        if (equation.length !== 0)
+            equation[getLength(equation)] = e.target.textContent;
+        isConcat = false;
+        return;
+    }
 
     if (isFirstOperand && isOperator && !isSecondOperand) {
         equation.push(Number(screen.textContent));
@@ -74,7 +109,7 @@ function calculate(e) {
 
         equation.push(e.target.textContent);
         isOperator = true;
-        isAppend=false;
+        isConcat = false;
     }
 
     if ((equation.length) % 3 === 0) {
@@ -86,14 +121,26 @@ function calculate(e) {
         isOperator = true;
         isFirstOperand = true;
         isSecondOperand = false;
-        isAppend=false;
+        isConcat = false;
     }
+
+    isClicked = false;
 }
 
 const equals = document.querySelector('button.equals');
 equals.addEventListener('click', equate);
 
-function equate() {
-
+function equate(e) {
+    const lastNum = Number(screen.textContent);
+    equation.push(lastNum);
+    length = getLength(equation) + 1;
+    if (length > 2) {
+        const equals = operate(equation[length - 2], equation[length - 3], equation[length - 1]);
+        screen.textContent = String(equals);
+        equation.push(equals);
+        isConcat = false;
+        isOperator = false;
+    }
+    isEquated = true;
 }
 
